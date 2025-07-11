60d03d2a07e4a70f901f70e265df1ef9316d78ad
devrok <roland.kurz@gmx.at>
2025-07-11 16:29:56 +0200

    textContent statt innerText

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,13 @@
 function eventListenerSearch() {
     const searchInput = window.getElementById("searchInput");
-    const searchTerm = window.escapeHtml(searchInput.value.trim());
+    const escapedSearchTerm = window.escapeHtml(searchInput.value.trim());
     const searchResult = window.getElementById("searchResult");
 
-    if (searchTerm) {
+    if (escapedSearchTerm) {
         // Hier könnte eine echte Suchfunktion implementiert werden
         //document.getElementById("searchResult").innerHTML = `Suchergebnisse für: <strong>${searchTerm}</strong>`;
         // mit innerText ist eine Verwendung von HTML nicht möglich
-        searchResult.innerText = `Suchergebnisse für: ${searchTerm}`;
+        searchResult.textContent = `Suchergebnisse für: ${escapedSearchTerm}`;
         searchResult.style.display = "block";
     } else {
         searchResult.style.display = "none";
